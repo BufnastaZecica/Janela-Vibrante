9750b04166427935bd3cc28da88172c8f4429b7f
BufnastaZecica <regovicsanja@gmail.com>
2022-03-15 19:20:31 +0100

    sem servisos

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ $(document).ready(function() {
         }
     });
 });
+
+function myFunction() {
+    var lermais = document.getElementById("more-hidden");
+    lermais.classList.toggle("open-more");
+    var butt = document.getElementById("hide");
+    butt.classList.toggle("hide");
+
+}
+
+function about() {
+    var quem = document.getElementById("more-hide");
+    quem.classList.toggle("show-more");
+    var buttn = document.getElementById("hiden");
+    buttn.classList.toggle("hiden");
+
+}
